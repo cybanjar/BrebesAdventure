27bf9b32d9007c5227c08e130730811c8a6d4b2a
cybanjar <cybanjar@gmail.com>
2021-10-09 23:31:29 +0700

    add navigation

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,7 +18,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({ children, title }): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
